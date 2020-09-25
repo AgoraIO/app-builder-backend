5d600acfd2b31e81092f9ed4cdc2a5209013809f
Samyak Jain <samtan106@gmail.com>
2020-09-26 00:33:25 +0530

    Fix PSTN bugs; Added better logging for PSTN

--- routes/pstn.go
+++ routes/pstn.go
@@ -2,11 +2,11 @@ package routes
 
 import (
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
 	"strings"
 
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 
 	"github.com/samyak-jain/agora_backend/models"
@@ -17,15 +17,15 @@ import (
 func (o *Router) DTMFHandler(w http.ResponseWriter, r *http.Request) {
 	dtmf := r.URL.Query().Get("id")
 
-	var channelData *models.Channel
+	var channelData models.Channel
 	if err := o.DB.Where("DTMF = ?", dtmf).First(&channelData).Error; err != nil {
-		log.Print(err)
+		log.Error().Err(err).Str("DTMF", dtmf).Msg("DTMF not found")
 	}
 
 	uid := int(rand.Uint32())
 	tokenData, err := utils.GetRtcToken(channelData.Name, uid)
 	if err != nil {
-		log.Print(err)
+		log.Error().Err(err).Msg("token generation failed")
 	}
 
 	responseString := `{
@@ -42,12 +42,12 @@ func (o *Router) DTMFHandler(w http.ResponseWriter, r *http.Request) {
 		]
 	}`
 
-	finalResult := strings.Replace(responseString, "{{AppID}}", viper.GetString("appID"), 1)
+	finalResult := strings.Replace(responseString, "{{AppID}}", viper.GetString("APP_ID"), 1)
 	finalResult = strings.Replace(finalResult, "{{Channel}}", channelData.Name, 1)
 	finalResult = strings.Replace(finalResult, "{{Token}}", tokenData, 1)
 	finalResult = strings.Replace(finalResult, "{{UID}}", fmt.Sprint(uid), 1)
 
-	log.Print(finalResult)
+	log.Info().Str("result", finalResult)
 	byt := []byte(finalResult)
 
 	w.Header().Set("Content-Type", "application/json")
