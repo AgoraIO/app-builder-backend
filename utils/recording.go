ca9c5db4d570d1991c108ad052fcc2c368feb4fa
Sandeep <rayanuthalas@gmail.com>
2020-12-17 02:27:47 +0530

    Revamped Recording

--- utils/recording.go
+++ utils/recording.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -21,6 +20,55 @@ type Recorder struct {
 	SID     string
 }
 
+type AcquireClientRequest struct {
+	ResourceExpiredHour int `json:"resourceExpiredHour,omitempty"`
+}
+
+type AcquireRequest struct {
+	Cname         string               `json:"cname"`
+	UID           string               `json:"uid"`
+	ClientRequest AcquireClientRequest `json:"clientRequest"`
+}
+
+type TranscodingConfig struct {
+	Height           int    `json:"height"`
+	Width            int    `json:"width"`
+	Bitrate          int    `json:"bitrate"`
+	Fps              int    `json:"fps"`
+	MixedVideoLayout int    `json:"mixedVideoLayout"`
+	BackgroundColor  string `json:"backgroundColor"`
+}
+
+type RecordingConfig struct {
+	MaxIdleTime       int               `json:"maxIdleTime"`
+	StreamTypes       int               `json:"streamTypes"`
+	ChannelType       int               `json:"channelType"`
+	DecryptionMode    int               `json:"decryptionMode,omitempty"`
+	Secret            string            `json:"secret,omitempty"`
+	TranscodingConfig TranscodingConfig `json:"transcodingConfig"`
+}
+
+type StorageConfig struct {
+	Vendor         int      `json:"vendor"`
+	Region         int      `json:"region"`
+	Bucket         string   `json:"bucket"`
+	AccessKey      string   `json:"accessKey"`
+	SecretKey      string   `json:"secretKey"`
+	FileNamePrefix []string `json:"fileNamePrefix"`
+}
+
+type ClientRequest struct {
+	Token           string          `json:"token"`
+	RecordingConfig RecordingConfig `json:"recordingConfig"`
+	StorageConfig   StorageConfig   `json:"storageConfig"`
+}
+
+type StartRecordRequest struct {
+	Cname         string        `json:"cname"`
+	UID           string        `json:"uid"`
+	ClientRequest ClientRequest `json:"clientRequest"`
+}
+
 // Acquire runs the acquire endpoint for Cloud Recording
 func (rec *Recorder) Acquire() error {
 	creds, err := GenerateUserCredentials(rec.Channel, false)
@@ -31,18 +79,16 @@ func (rec *Recorder) Acquire() error {
 	rec.UID = creds.UID
 	rec.Token = creds.Rtc
 
-	requestBody := fmt.Sprintf(`
-		{
-			"cname": "%s",
-			"uid": "%d",
-			"clientRequest": {
-				"resourceExpiredHour": 24
-			}
-		}
-	`, rec.Channel, rec.UID)
+	requestBody, err := json.Marshal(&AcquireRequest{
+		Cname: rec.Channel,
+		UID:   string(rec.UID),
+		ClientRequest: AcquireClientRequest{
+			ResourceExpiredHour: 24,
+		},
+	})
 
 	req, err := http.NewRequest("POST", "https://api.agora.io/v1/apps/"+viper.GetString("APP_ID")+"/cloud_recording/acquire",
-		bytes.NewBuffer([]byte(requestBody)))
+		bytes.NewBuffer(requestBody))
 	if err != nil {
 		return err
 	}
@@ -69,79 +115,58 @@ func (rec *Recorder) Acquire() error {
 func (rec *Recorder) Start(secret *string) error {
 	currentTime := strconv.FormatInt(time.Now().Unix(), 10)
 
-	var requestBody string
+	transcodingConfig := TranscodingConfig{
+		Height:           720,
+		Width:            1280,
+		Bitrate:          2260,
+		Fps:              15,
+		MixedVideoLayout: 1,
+		BackgroundColor:  "#000000",
+	}
+	var recordingConfig RecordingConfig
 	if secret != nil && *secret != "" {
-		requestBody = fmt.Sprintf(`
-		{
-			"cname": "%s",
-			"uid": "%d",
-			"clientRequest": {
-				"token": "%s",
-				"recordingConfig": {
-					"maxIdleTime": 30,
-					"streamTypes": 2,
-					"channelType": 1,
-					"decryptionMode": 1,
-					"secret": "%s",
-					"transcodingConfig": {
-						"height": 720, 
-						"width": 1280,
-						"bitrate": 2260, 
-						"fps": 15, 
-						"mixedVideoLayout": 1,
-						"backgroundColor": "#000000"
-					}
-				},
-				"storageConfig": {
-					"vendor": 1, 
-					"region": 0,
-					"bucket": "%s",
-					"accessKey": "%s",
-					"secretKey": "%s",
-					"fileNamePrefix": ["%s", "%s"]
-				}
-			}
+
+		recordingConfig = RecordingConfig{
+			MaxIdleTime:       30,
+			StreamTypes:       2,
+			ChannelType:       1,
+			DecryptionMode:    1,
+			Secret:            *secret,
+			TranscodingConfig: transcodingConfig,
 		}
-	`, rec.Channel, rec.UID, rec.Token, *secret, viper.GetString("BUCKET_NAME"),
-			viper.GetString("BUCKET_ACCESS_KEY"), viper.GetString("BUCKET_ACCESS_SECRET"),
-			rec.Channel, currentTime)
 	} else {
-		requestBody = fmt.Sprintf(`
-		{
-			"cname": "%s",
-			"uid": "%d",
-			"clientRequest": {
-				"token": "%s",
-				"recordingConfig": {
-					"maxIdleTime": 30,
-					"streamTypes": 2,
-					"channelType": 1,
-					"transcodingConfig": {
-						"height": 720, 
-						"width": 1280,
-						"bitrate": 2260, 
-						"fps": 15, 
-						"mixedVideoLayout": 1,
-						"backgroundColor": "#000000"
-					}
-				},
-				"storageConfig": {
-					"vendor": 1, 
-					"region": 0,
-					"bucket": "%s",
-					"accessKey": "%s",
-					"secretKey": "%s",
-					"fileNamePrefix": ["%s", "%s"]
-				}
-			}
+		recordingConfig = RecordingConfig{
+			MaxIdleTime:       30,
+			StreamTypes:       2,
+			ChannelType:       1,
+			TranscodingConfig: transcodingConfig,
 		}
-	`, rec.Channel, rec.UID, rec.Token, viper.GetString("BUCKET_NAME"),
-			viper.GetString("BUCKET_ACCESS_KEY"), viper.GetString("BUCKET_ACCESS_SECRET"),
-			rec.Channel, currentTime)
+	}
+
+	requestBody, err := json.Marshal(&StartRecordRequest{
+		Cname: rec.Channel,
+		UID:   string(rec.UID),
+		ClientRequest: ClientRequest{
+			Token: rec.Token,
+			StorageConfig: StorageConfig{
+				Vendor:    viper.GetInt("RECORDING_VENDOR"),
+				Region:    viper.GetInt("RECORDING_REGION"),
+				Bucket:    viper.GetString("BUCKET_NAME"),
+				AccessKey: viper.GetString("BUCKET_ACCESS_KEY"),
+				SecretKey: viper.GetString("BUCKET_ACCESS_SECRET"),
+				FileNamePrefix: []string{
+					rec.Channel, currentTime,
+				},
+			},
+			RecordingConfig: recordingConfig,
+		},
+	})
+	if err != nil {
+		return err
 	}
 
 	req, err := http.NewRequest("POST", "https://api.agora.io/v1/apps/"+viper.GetString("APP_ID")+"/cloud_recording/resourceid/"+rec.RID+"/mode/mix/start",
-		bytes.NewBuffer([]byte(requestBody)))
+		bytes.NewBuffer(requestBody))
 	if err != nil {
 		return err
 	}
@@ -165,14 +190,11 @@ func (rec *Recorder) Start(secret *string) error {
 
 // Stop stops the cloud recording
 func Stop(channel string, uid int, rid string, sid string) error {
-	requestBody := fmt.Sprintf(`
-		{
-			"cname": "%s",
-			"uid": "%d",
-			"clientRequest": {
-			}
-		}
-	`, channel, uid)
+	requestBody, err := json.Marshal(&AcquireRequest{
+		Cname:         channel,
+		UID:           string(uid),
+		ClientRequest: AcquireClientRequest{},
+	})
 
 	req, err := http.NewRequest("POST", "https://api.agora.io/v1/apps/"+viper.GetString("APP_ID")+"/cloud_recording/resourceid/"+rid+"/sid/"+sid+"/mode/mix/stop",
 		bytes.NewBuffer([]byte(requestBody)))
