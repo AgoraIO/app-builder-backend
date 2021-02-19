dfe69de542acb960409ffd776720e0fdd394d9ab
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-02-19 12:54:12 +0530

    Added Auto Migrate

--- migrations/init.go
+++ migrations/init.go
@@ -1,22 +1,32 @@
 package migrations
 
 import (
-	"log"
-
-	"github.com/samyak-jain/agora_backend/models"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/samyak-jain/agora_backend/utils"
+	"log"
 )
 
 // RunMigration runs the schema migrations
 func RunMigration() {
 	utils.SetupConfig()
-	db, err := models.CreateDB(utils.GetDBURL())
+	//db, err := models.CreateDB(utils.GetDBURL())
+	//if err != nil {
+	//	log.Print(err)
+	//	return
+	//}
+	//
+	//defer db.Close()
+
+	//db.AutoMigrate(&models.User{}, &models.Channel{}, &models.Token{})
+	m, err := migrate.New(
+		"file://migrations",
+		utils.GetDBURL())
 	if err != nil {
-		log.Print(err)
-		return
+		log.Fatal(err)
 	}
-
-	defer db.Close()
-
-	db.AutoMigrate(&models.User{}, &models.Channel{}, &models.Token{})
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
 }
