c5c3f99a9f59b24a55d1836ad06c5b8255a969f6
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-03-17 19:07:16 +0530

    Added Random UInt32 Instead of UID as 0

--- utils/tokens_test.go
+++ utils/tokens_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/AccessToken"
 	rtctokenbuilder "github.com/samyak-jain/agora_backend/utils/rtctoken"
 	rtmtokenbuilder "github.com/samyak-jain/agora_backend/utils/rtmtoken"
+	"math/rand"
 	"testing"
 )
 
@@ -11,9 +12,9 @@ func Test_RtcTokenBuilder(t *testing.T) {
 	appID := "970CA35de60c44645bbae8a215061b33"
 	appCertificate := "5CFd2fd1755d40ecb72977518be15d3b"
 	channelName := "7d72365eb983485397e3e3f9d460bdda"
-	uidZero := uint32(0)
-	expiredTs := uint32(1446455471)
-	result, err := rtctokenbuilder.BuildTokenWithUID(appID, appCertificate, channelName, uidZero, rtctokenbuilder.RoleSubscriber, expiredTs)
+	uid := rand.Uint32()
+	expiredTs := uint32(14471)
+	result, err := rtctokenbuilder.BuildTokenWithUID(appID, appCertificate, channelName, uid, rtctokenbuilder.RoleSubscriber, expiredTs)
 
 	if err != nil {
 		t.Error(err)
