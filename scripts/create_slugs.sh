fb51def02679dc100c92548440da9933a38d5669
girishg4t <talekar.g@gmail.com>
2022-01-19 15:13:17 +0530

    corrected script

--- scripts/create_slugs.sh
+++ scripts/create_slugs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir app 
 cd app
-GOARCH=amd64 GOOS=linux go build ../../cmd/video_conferencing/server.go 
+GOARCH=amd64 GOOS=linux go build ../cmd/video_conferencing/server.go 
 cd ..
 tar czfv slug.tgz ./app
